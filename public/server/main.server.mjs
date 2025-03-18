0dde2ec8ddc05157a271b3ff70bbea2f055727e8
Gabriel Toledo <gabrielreb7@gmail.com>
2025-03-18 09:49:03 -0300

    ip front

--- public/server/main.server.mjs
+++ public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KCT3VXKE.mjs";import"./chunk-EBI5CDJC.mjs";import"./chunk-JTQC6PDZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4YOVYQQ3.mjs";import"./chunk-EBI5CDJC.mjs";import"./chunk-JTQC6PDZ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
