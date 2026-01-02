3f719d87052f483b407648329789cf466f474a89
Gabriel Toledo <gabrielreb7@gmail.com>
2026-01-02 14:39:58 -0300

    logs

--- src/modules/inventario/services/busca-inventario.service.js
+++ src/modules/inventario/services/busca-inventario.service.js
@@ -10,14 +10,69 @@ class CapturaComposicaoService {
 
     async buscarInventariosLiberadosNaoImportados() {
 
-       // const sql = `SELECT * FROM INVENTARIO_ESTOQUE  WHERE TABLET_IMPORTOU = 'N' AND TABLET_IMPORTAR ='S'`;
+        const sql = `SELECT ID_INVENTARIO, DATA FROM INVENTARIO_ESTOQUE  WHERE (TABLET_IMPORTOU = 'N' OR TABLET_IMPORTOU IS NULL)  AND TABLET_IMPORTAR ='S'`;
 
-       const sql = `SELECT FIRST 10 *
-                    FROM INVENTARIO_ESTOQUE`
+        //const sql = `SELECT FIRST 10 * FROM INVENTARIO_ESTOQUE`
 
 
         const rows = await queryFirebird(sql, []);
 
+        /* INVENTARIO_ESTOQUE
+        { 
+            ID_INVENTARIO: 2,
+            DATA: 2025-12-31T00:00:00.000Z,
+            USUARIO: 'ADMINISTRADOR',
+            STATUS_PROCESSADO: 'N',
+            HORA: '08:34:11',
+            DATA_PROCESSAMENTO: null,
+            HORA_PROCESSAMENTO: null,
+            QUEM_PROCESSOU: null,
+            TABLET_IMPORTAR: 'S',
+            TABLET_IMPORTOU: null
+        }
+        */
+
+        console.log("Cheguei aqui! Inventario")
+        console.log(rows)
+
+        for (const row of rows) {
+
+
+            const datas = {
+                id: Number(row.ID_INVENTARIO),
+                data: row.DATA,
+                status: 'Aberto',
+            }
+
+            try {
+
+                console.log("-------------CABEÇALHO INICIO------------------")
+                console.log(datas)
+                console.log("-------------CABEÇALHO FIM------------------")
+                await this.buscarProdutosPorOps(row.ID_INVENTARIO);
+
+                /*  const create = container.resolve(CreateCabSeparacaoService);
+                 const data = await create.executeMaxibom(datas);
+                 console.log(data);
+ 
+                 if (data) {
+                     await this.buscarProdutosPorOps(row.ID_PRODUCAO);
+ 
+                     await this.updateOPImportou(row.ID_PRODUCAO);
+ 
+                 } */
+
+            } catch (e) {
+                console.error("Ocorreu um erro ao inserir op: " + row.ID_PRODUCAO);
+            }
+        }
+
+    }
+
+    async buscarProdutosPorOps(id) {
+        const sql = ` SELECT ID_INVENTARIO_ITENS,ID_INVENTARIO, COD_PRO,  DESC_PRO, QUANT, LOCAL_ARMAZENAGEM, ESTOQUE, DIFERENCA, TIPO_ITEM, SIMB_UND FROM INVENTARIO_ITENS_ESTOQUE WHERE ID_INVENTARIO = ?`;
+
+        const rows = await queryFirebird(sql, [id]);
 
 
         /* INVENTARIO_ITENS_ESTOQUE
@@ -38,70 +93,35 @@ class CapturaComposicaoService {
             */
 
 
-        console.log("Cheguei aqui! Inventario")
-        console.log(rows)
-
-        for (const row of rows) {
-
-
-            /* const datas = {
-                id: Number(row.ID_PRODUCAO),
-                idOp: Number(row.ID_PRODUCAO),
-                idprod: row.COD_ITEM,
-                qtdeprod: row.QUANT_KG,
-                status: 3,
-                observacao: '',
-                name: row.DESC_PRO
-            } */
-            try {
-                /*  const create = container.resolve(CreateCabSeparacaoService);
-                 const data = await create.executeMaxibom(datas);
-                 console.log(data);
- 
-                 if (data) {
-                     await this.buscarProdutosPorOps(row.ID_PRODUCAO);
- 
-                     await this.updateOPImportou(row.ID_PRODUCAO);
- 
-                 } */
-
-            } catch (e) {
-                console.error("Ocorreu um erro ao inserir op: " + row.ID_PRODUCAO);
-            }
-        }
-
-    }
-
-    async buscarProdutosPorOps(id) {
-        const sql = ` 
-        select 
-       ITENS.*
-        from INVENTARIO_ESTOQUE INV join INVENTARIO_ITENS_ESTOQUE ITENS
-        on INV.ID_INVENTARIO = ITENS.ID_INVENTARIO
-        where   INV.TABLET_IMPORTOU = 'N' AND INV.TABLET_IMPORTAR ='S'
-        order by ITENS.COD_PRO
-        `;
-
-        const rows = await queryFirebird(sql, []);
-
         console.log("Itens: [....]")
         console.log(rows)
 
         for (const row of rows) {
 
 
+            const datas = {
+                idCabInvent: Number(row.ID_INVENTARIO),
+                idItem: Number(row.ID_INVENTARIO_ITENS),
+                idprod: row.COD_PRO,
+                codprod: row.DESC_PRO,
+                descricaoprod: row.DESC_PRO,
+                quantidade: row.QUANT,
+                local: row.LOCAL_ARMAZENAGEM,
+                estoque: row.ESTOQUE,
+                diferenca: 0,
+                tipoitem: row.TIPO_ITEM,
+                unidade: row.SIMB_UND,
+            }
 
 
-            /* const datas = {
-                idCabSep: Number(row.ID_SOLICITACAO),
-                idprod: row.COD_CPRO,
-                name: row.DESC_PRO,
-                qtdeaseparar: row.QUANT_USADA,
-                qtdeseparada: 0,
-                qtdeconferida: 0,
-                codvol: row.DESC_UND
-            } */
+
+
             try {
+
+                console.log("-------------ITENS INICIO------------------")
+                console.log(datas)
+                console.log("-------------ITENS FIM------------------")
+
                 /*  const create = container.resolve(CreateItensSeparacaoService);
                  const data = await create.execute(datas);
                  console.log(data);
@@ -111,7 +131,7 @@ class CapturaComposicaoService {
                  } */
 
             } catch (e) {
-                console.error("Ocorreu um erro ao inserir op: " + row.ID_PRODUCAO);
+                console.error("Ocorreu um erro ao inserir inventário: " + row.ID_INVENTARIO);
             }
         }
 
